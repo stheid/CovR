41c33d71257fa1c3ffbfa17b1eba3b6d72eb1439
Stefan Heid <sheid@mail.upb.de>
2020-05-10 19:01:45 +0200

    added simple setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='CovR',
+      version='0.0.1',
+      description='Simple flask project to create a Cov19 reproduction rate R in germany (Data source RKI)',
+      url='https://github.com/stheid/CovR',
+      author='Stefan Heid',
+      author_email='stefan.heid@upb.de',
+      license='MIT',
+      zip_safe=False)
