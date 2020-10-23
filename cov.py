e1ca7366c36b4534b5de5d1ca42d254f8d8f1a95
Stefan Heid <stefan.heid@upb.de>
2020-10-23 13:45:58 +0200

    added flask caching

--- cov.py
+++ cov.py
@@ -1,14 +1,22 @@
 from flask import Flask, send_file, render_template
+from flask_caching import Cache
 
 from model import gen_plot
 
+cache = Cache(config={'CACHE_TYPE': 'simple'})
 app = Flask(__name__)
 
+cache.init_app(app)
+
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
+
+
 @app.route("/img/covr")
+@cache.cached(timeout=60 * 60)
 def images():
     buffer = gen_plot()
     return send_file(buffer, mimetype='image/svg+xml')
+
 
 @app.route("/")
 def main():
